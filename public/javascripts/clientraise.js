8a6c829a83de2028344e1183998957752cc2a1c8
Raunak <rskalsi39@gmail.com>
2023-02-12 02:53:11 +0530

    raise req

--- public/javascripts/clientraise.js
+++ public/javascripts/clientraise.js
@@ -1,15 +1,37 @@
-const alldiv = document.getElementsByClassName('col-sm')
-const hiddiv = document.getElementsByClassName('hidtxt')
+const paintdiv = document.getElementById('paint-div');
+const masondiv = document.getElementById('mason-div');
+const plumbdiv = document.getElementById('plumb-div');
+const carpentdiv = document.getElementById('carpent-div');
+const elecdiv = document.getElementById('elec-div');
+const labourdiv = document.getElementById('labour-div');
+const housediv = document.getElementById('house-div');
+const pestdiv = document.getElementById('pest-div');
 
-// function show(b) {
-//     console.log("fire")
-//     b.classList.remove('hidden')
-// }
+const painttxt = document.getElementById('paint-txt');
+const masontxt = document.getElementById('mason-txt');
+const plumbtxt = document.getElementById('plumb-txt');
+const carpenttxt = document.getElementById('carpent-txt');
+const electxt = document.getElementById('elec-txt');
+const labourtxt = document.getElementById('labour-txt');
+const housetxt = document.getElementById('house-txt');
+const pesttxt = document.getElementById('pest-txt');
 
-for (i = 0; i < 8; i++) {
-    console.log(hiddiv[i])
-    alldiv[i].addEventListener('mouseenter', () => {
-    hiddiv[i].classList.remove('hidden')
-          
-})
+const eventmaker = (div, txt) => {
+    div.addEventListener('mouseenter', () => {
+        txt.classList.remove('hidden');
+    })
+
+    div.addEventListener('mouseleave', () => {
+        txt.classList.add('hidden');
+    })
 }
+
+eventmaker(paintdiv,painttxt)
+eventmaker(masondiv,masontxt)
+eventmaker(plumbdiv,plumbtxt)
+eventmaker(carpentdiv,carpenttxt)
+eventmaker(elecdiv,electxt)
+eventmaker(labourdiv,labourtxt)
+eventmaker(housediv,housetxt)
+eventmaker(pestdiv,pesttxt)
+
