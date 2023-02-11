71fb674e56e0dac292e0c6ffac12abc8909c6018
Raunak <rskalsi39@gmail.com>
2023-02-11 19:50:39 +0530

    worker signup done

--- public/javascripts/workersignup.js
+++ public/javascripts/workersignup.js
@@ -1,8 +1,23 @@
+const linkshow = document.getElementById('linktext')
+
 const fileupload = () => {
-    console.log("hello")
     const client = filestack.init("AqIhMj2cQRZqk3hoxPVesz");
     const options = {
-        fromSources: ["local_file_system", "instagram", "facebook"],
+        fromSources: ["local_file_system"],
+        accept: ['.png', '.jpg', '.jpeg'],
+        onUploadDone: file => {
+            linkshow.classList.remove('hidden');
+            linkshow.value = file.filesUploaded[0].url;
+        },
+        acceptFn: (file, options) => {
+            const mimeFromExtension = options.mimeFromExtension(file.originalFile.name);
+            if (options.acceptMime.length && !options.acceptMime.includes(mimeFromExtension)) {
+                return Promise.reject('Cannot accept that file')
+            }
+            return Promise.resolve()
+        },
     };
     client.picker(options).open();
 }
+
+
